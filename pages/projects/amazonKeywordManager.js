55ec3f8745ff07c07ad3a6fdf2e8c74f3d2447e2
Daniel Hajek <danielhajek85@gmail.com>
2023-08-03 13:11:01 +0200

    writing article on amazon genius

--- pages/projects/amazonKeywordManager.js
+++ pages/projects/amazonKeywordManager.js
@@ -3,8 +3,151 @@ import Link from "next/link";
 export default function amazonKeywordManager() {
   return (
     <Layout>
-      <h2>project related to keyword management used by amazon sellers</h2>
-      <Link href="/projects" className="p-1 shadow rounded-lg">back to projects</Link>
+      <div className="max-w-3xl mx-auto px-4 py-8">
+        <h1 className=" text-3xl font-semibold m-3">
+          Building Amazon Keyword Genius: Empowering Amazon Self-Publishers with
+          Advanced Keyword Research
+        </h1>
+        <div className="m-3">
+          <p>
+            In today's digital age, the realm of self-publishing has opened up
+            immense opportunities for aspiring authors and content creators to
+            share their work with the world. However, navigating the Amazon
+            marketplace and ensuring your content reaches the right audience can
+            be a challenging endeavor. Enter "Amazon Keyword Genius," a
+            groundbreaking web app platform designed to revolutionize the
+            keyword research process for Amazon self-publishers. In this
+            article, we'll dive into the technical intricacies behind this
+            innovative tool and explore the key features that make it a
+            game-changer in the self-publishing landscape.
+          </p>
+
+          <h2 className="font-bold my-2">
+            Genesis of Amazon Keyword Genius: A Friend's Challenge
+          </h2>
+          <p>
+            The inception of Amazon Keyword Genius can be traced back to a
+            conversation with a friend deeply involved in the world of Amazon
+            self-publishing. Faced with the complex task of optimizing keywords
+            to enhance discoverability, my friend's challenges became the
+            driving force behind the creation of a sophisticated software
+            solution. The aim was clear: to develop a comprehensive platform
+            that simplifies and streamlines keyword research while providing a
+            range of additional utilities to empower self-publishers.
+          </p>
+          <h2 className="font-bold my-2">
+            Technical Architecture: A Robust Foundation
+          </h2>
+          <p>
+            Amazon Keyword Genius is built on a solid technical foundation,
+            combining the latest advancements in software development to deliver
+            a seamless user experience. The platform's architecture follows the
+            proven "Onion Architecture" pattern, which separates the application
+            into distinct layers, each with its own responsibilities and
+            boundaries.
+          </p>
+          <ul className=" p-5">
+            <li>
+              <b>Core:</b> The heart of the application, the Core layer houses
+              essential domain models that define the structure and logic of the
+              system.
+            </li>
+            <li>
+              <b>Data:</b> This layer handles all interactions with the SQL
+              Server database, utilizing Entity Framework Core 6 to manage data
+              access. It also includes mappers and helpers for efficient data
+              transformation.
+            </li>
+            <li>
+              <b>Services:</b> Business logic takes center stage in the Services
+              layer. Services, injected into various components, drive the core
+              functionalities of the application.
+            </li>
+          </ul>
+          <h2 className="font-bold my-2">
+            Authentication and Security: IdentityServer 4
+          </h2>
+          <p>
+            Ensuring the security and privacy of users' data is paramount.
+            Amazon Keyword Genius implements a robust security layer using
+            IdentityServer 4. This enables users to securely log in using their
+            social accounts, such as Facebook or Amazon credentials, while
+            maintaining the integrity of their personal information.
+          </p>
+          <h2 className="font-bold my-2">
+            Frontend and State Management: ReactJS and Redux
+          </h2>
+          <p>
+            The frontend of Amazon Keyword Genius is crafted using the powerful
+            ReactJS library, providing a dynamic and responsive user interface.
+            Redux is employed for efficient state management, ensuring seamless
+            communication between components and a fluid user experience.
+          </p>
+          <h2 className="font-bold my-2">
+            Key Features: Unlocking the Power of Amazon Keyword Genius
+          </h2>
+          <p>
+            Amazon Keyword Genius isn't just about its architectural elegance;
+            it's about delivering tangible value to self-publishers. Let's delve
+            into some of its standout features:
+          </p>
+          <ul className=" p-5">
+            <li>
+              User Login: Seamlessly log in with social accounts, ensuring a
+              hassle-free onboarding experience.
+            </li>
+            <li>
+              Payment Integration: Purchase packages and credits using Stripe
+              SDK, enabling secure and convenient transactions.
+            </li>
+            <li>
+              Keyword Research: Leverage ForestApis and Amazon APIs to conduct
+              comprehensive keyword research, gaining valuable insights to
+              optimize content discoverability.
+            </li>
+            <li>
+              CSV Import and Export: Easily import and export keyword data in
+              CSV format, simplifying data management.
+            </li>
+            <li>
+              Webhooks: Utilize Stripe webhooks for real-time payment updates
+              and enhanced payment processing.
+            </li>
+            <li>
+              Dashboard: Empower users with a comprehensive dashboard to
+              customize backend job frequencies, optimizing the asynchronous
+              processes that drive the platform.
+            </li>
+            <li>
+              Credits Management: Manage credits efficiently based on user
+              preferences, providing granular control over the keyword research
+              process.
+            </li>
+          </ul>
+          <h2 className="font-bold my-2">
+            Conclusion: Empowering Self-Publishers in the Amazon Universe
+          </h2>
+          <p>
+            Amazon Keyword Genius stands as a testament to the power of
+            innovation and collaboration. What began as a solution to a friend's
+            challenges has evolved into a cutting-edge web app platform that
+            empowers self-publishers with advanced keyword research
+            capabilities, seamless user experiences, and efficient data
+            management. By combining a robust technical architecture with
+            user-centric features, Amazon Keyword Genius is poised to reshape
+            the landscape of Amazon self-publishing, unlocking new avenues of
+            success for content creators worldwide.
+          </p>
+        </div>
+        <div>
+          <Link
+            href="/projects"
+            className="p-1 bg-slate-950 text-white shadow rounded-lg"
+          >
+            back to projects
+          </Link>
+        </div>
+      </div>
     </Layout>
   );
 }
